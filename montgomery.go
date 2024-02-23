20595dca45269efb40702c937cd13ba2cfe39549
liuzhi <apisdk@vip.qq.com>
2024-02-23 22:14:22 +0800

    fix bug

--- montgomery.go
+++ montgomery.go
@@ -58,7 +58,7 @@ func NP0(m *big.Int) uint32 {
 	var modulus C.mpz_t
 	var np0 C.uint
 	C.mpz_init(&modulus[0])
-	C.hex_to_mpz((*C.char)(C.CBytes(m.Bytes())), (*C.mpz_t)(unsafe.Pointer(&modulus[0])))
+	C.hex_to_mpz((*C.char)(C.CBytes([]byte(m.Text(16)))), (*C.mpz_t)(unsafe.Pointer(&modulus[0])))
 	np0 = C.find_np0(&modulus[0])
 	C.mpz_clear(&modulus[0])
 	return uint32(np0)
@@ -70,9 +70,9 @@ func powm_odd(base, exp, mod *big.Int) (*big.Int, error) {
 	var r *C.char
 	var n C.int
 	C.mpz_init(&rop[0])
-	C.hex_to_mpz((*C.char)(C.CBytes(base.Bytes())), (*C.mpz_t)(unsafe.Pointer(&b[0])))
-	C.hex_to_mpz((*C.char)(C.CBytes(exp.Bytes())), (*C.mpz_t)(unsafe.Pointer(&e[0])))
-	C.hex_to_mpz((*C.char)(C.CBytes(mod.Bytes())), (*C.mpz_t)(unsafe.Pointer(&m[0])))
+	C.hex_to_mpz((*C.char)(C.CBytes([]byte(base.Text(16)))), (*C.mpz_t)(unsafe.Pointer(&b[0])))
+	C.hex_to_mpz((*C.char)(C.CBytes([]byte(exp.Text(16)))), (*C.mpz_t)(unsafe.Pointer(&e[0])))
+	C.hex_to_mpz((*C.char)(C.CBytes([]byte(mod.Text(16)))), (*C.mpz_t)(unsafe.Pointer(&m[0])))
 	C.mpz_powm(&rop[0], &b[0], &e[0], &m[0])
 	n = C.mpz_to_hex(&rop[0], &r)
 	br := C.GoBytes(unsafe.Pointer(r), n)
